30f6250a5049ad277ee19a38074c69aea1139c02
devanshijoshi9 <devanshijoshi1999@gmail.com>
2022-06-07 14:34:11 +0530

    Props usage and example to pass data and show it dynamically

--- src/App.js
+++ src/App.js
@@ -3,22 +3,46 @@ import './App.css';
 import ExpenseItem from'./components/ExpenseItem'; 
 
 function App() {
+
+  const expenses = [
+    { 
+      id: 1,
+      title: 'Mobile Recharge',
+      amount: 700,
+      date: new Date(2021, 4, 15)
+    },
+    { 
+      id: 2,
+      title: 'Gas Bill',
+      amount: 1000,
+      date: new Date(2021, 4, 20)
+    },
+    { 
+      id: 1,
+      title: 'Electricity Bill',
+      amount: 500,
+      date: new Date(2021, 5, 13)
+    }
+  ]
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <ExpenseItem></ExpenseItem>
+        <ExpenseItem
+          title  = {expenses[0].title}
+          amount = {expenses[0].amount}
+          date   = {expenses[0].date}
+        ></ExpenseItem>
+        <ExpenseItem
+          title  = {expenses[1].title}
+          amount = {expenses[1].amount}
+          date   = {expenses[1].date}
+        ></ExpenseItem>
+        <ExpenseItem
+          title  = {expenses[2].title}
+          amount = {expenses[2].amount}
+          date   = {expenses[2].date}
+        ></ExpenseItem>
       </header>
     </div>
   );
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,16 @@
-function ExpenseItem() {
-    return <h2>Expense Items!</h2>
+import './ExpenseItem.css';
+
+function ExpenseItem( props ) {
+
+    return (
+        <div className='expense-item'>
+            <div>{props.date.toISOString()}</div>
+            <div className='expense-item__description'>
+                <h2>{props.title}</h2>
+                <div className='expense-item__price'>{props.amount} Rs.</div>
+            </div>
+        </div>
+    );
 }
 
 export default ExpenseItem;
