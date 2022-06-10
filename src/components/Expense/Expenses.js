0d9177a0a0cb4116307c4d8a580f9b9f0b8caedc
devanshijoshi9 <devanshijoshi1999@gmail.com>
2022-06-10 15:59:56 +0530

    Show Expense data dynamic

--- src/components/Expense/Expenses.js
+++ src/components/Expense/Expenses.js
@@ -12,31 +12,17 @@ const Expenses = (props) => {
     setFilteredYear(selectedYear)
   };
 
-  const expenses = [
-    { 
-      id: 1,
-      title: 'Mobile Recharge',
-      amount: 700,
-      date: new Date(2021, 4, 15)
-    },
-    { 
-      id: 2,
-      title: 'Gas Bill',
-      amount: 1000,
-      date: new Date(2021, 4, 20)
-    },
-    { 
-      id: 1,
-      title: 'Electricity Bill',
-      amount: 500,
-      date: new Date(2021, 5, 13)
-    }
-  ];
-
   return (
     <Card className="expenses">
         <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-        <ExpenseItem
+        {props.items.map((expense) => (
+          <ExpenseItem
+              title={expense.title}
+              amount={expense.amount}
+              date={expense.date}
+          />
+        ))}
+        {/* <ExpenseItem
           title  = {expenses[0].title}
           amount = {expenses[0].amount}
           date   = {expenses[0].date}
@@ -50,7 +36,7 @@ const Expenses = (props) => {
           title  = {expenses[2].title}
           amount = {expenses[2].amount}
           date   = {expenses[2].date}
-        />
+        /> */}
     </Card>
   );
 }
