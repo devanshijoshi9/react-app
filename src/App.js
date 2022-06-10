918f357719220e142bf510f9c7122e218829abbe
devanshijoshi9 <devanshijoshi1999@gmail.com>
2022-06-10 10:54:03 +0530

    Add useState to form and get data when form submit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
-import ExpenseItem from './components/ExpenseItem';
+import Expense from './components/Expense/Expenses'
+import NewExpense from './components/NewExpense/NewExpense';
 
-function App() {
+const App = () => {
+
+  const expenses = [
+    { 
+      id: 1,
+      title: 'Mobile Recharge',
+      amount: 700,
+      date: new Date(2021, 4, 15)
+    },
+    { 
+      id: 2,
+      title: 'Gas Bill',
+      amount: 1000,
+      date: new Date(2021, 4, 20)
+    },
+    { 
+      id: 1,
+      title: 'Electricity Bill',
+      amount: 500,
+      date: new Date(2021, 5, 13)
+    }
+  ];
+
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem></ExpenseItem>
+      <NewExpense />
+      <Expense items={expenses} />
     </div>
   );
 }
